d60e6c4c69bc7f1952bc3a7a8fd67c20af06b377
mkaloukh <mohammadreza.kaloukh@freenet.ag>
2022-07-06 12:10:06 +0200

    Second changes main dev

--- src/Calss_One.java
+++ src/Calss_One.java
@@ -1,7 +1,7 @@
 public class Calss_One {
     public static void main(String[] args) {
         //I add this changes in first version
-        String sampleText = "This is my sample text one";
+        String sampleText = "This is my sample text one that has changed recently";
         System.out.println(sampleText);
         //+++++++++++++++++++++++++++++++++++++++++++++++++
 
