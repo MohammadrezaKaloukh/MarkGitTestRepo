367c9882b7437df6e190e3dc2bb4ad032e373fe3
mkaloukh <mohammadreza.kaloukh@freenet.ag>
2022-07-06 11:58:17 +0200

    First changes

---
+++ src/Calss_One.java
@@ -0,0 +1,9 @@
+public class Calss_One {
+    public static void main(String[] args) {
+        //I add this changes in first version
+        String sampleText = "This is my sample text one";
+        System.out.println(sampleText);
+        //+++++++++++++++++++++++++++++++++++++++++++++++++
+
+    }
+}
